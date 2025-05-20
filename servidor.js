b7dba46c718bbcd92b57ad1c9b12708d40f0d4da
frikingsoft <nicolas.barone1@gmail.com>
2025-05-20 20:40:54 -0300

    compartiendo pagina web, configurando archivos estaticos

--- servidor.js
+++ servidor.js
@@ -1,11 +1,13 @@
 const express = require("express") // LLamamos al modulo express
-
+const path = require("path")
 const servidor = express() // ejecutamos el modulo de express
 
 servidor.listen(80) // Servidor escuchando en el puerto 80 
 // GET POST PUT DELETE
+let pagina=path.join(__dirname, "frontend")
+servidor.use(express.static(pagina))
 servidor.get("/",(req,res)=>{
-    
+    res.sendFile(path.join(__dirname , "/frontend/index.html"))
 })
 servidor.get("/usuario",(req,res)=>{
    
