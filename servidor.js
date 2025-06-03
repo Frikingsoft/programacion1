68d4f0d6f9fd3a9806250d1778fa84e616cd9fbb
frikingsoft <nicolas.barone1@gmail.com>
2025-06-03 20:09:09 -0300

    Cambios3-6

--- servidor.js
+++ servidor.js
@@ -1,13 +1,16 @@
 const express = require("express") // LLamamos al modulo express
 const path = require("path")
+const hbs = require("hbs")
 const servidor = express() // ejecutamos el modulo de express
 
 servidor.listen(80) // Servidor escuchando en el puerto 80 
 // GET POST PUT DELETE
-let pagina=path.join(__dirname, "frontend")
+let pagina=path.join(__dirname, "views")
 servidor.use(express.static(pagina))
+servidor.set("view engine", "hbs")
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 servidor.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname , "/frontend/index.html"))
+    res.render("index.hbs")
 })
 servidor.get("/usuario",(req,res)=>{
    
